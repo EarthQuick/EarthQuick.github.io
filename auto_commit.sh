6b2623ede991779336d39b7133ea19586fdba3cb
SergioChan <yuheng9211@qq.com>
2016-11-17 09:19:43 +0800

    Auto update 2016-11-17-09:19:43

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,9 +1,11 @@
 cd /Users/yhc/Documents/EarthQuick.github.io
+
+git config user.email "yuheng9211@qq.com"
+git config user.name "SergioChan"
+
 git add .
 git add ../data/
 git add ../
-git config --global user.email "yuheng9211@qq.com"
-git config --global user.name "SergioChan"
 
 CURTIME=$(date "+%Y-%m-%d-%H:%M:%S")
 git commit -m 'Auto update '${CURTIME}
